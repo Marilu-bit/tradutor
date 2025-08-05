3aa65428f38a473c2b2012d41521d07bd4a35209
Maria <nami52798@gmail.com>
2025-08-05 19:06:58 -0300

    Tentativa 2

--- static/javascript.js
+++ static/javascript.js
@@ -22,7 +22,7 @@ function BotaoTraduzir(){
             return;
         }
 
-        fetch("http://127.0.0.1:5000", { 
+        fetch("//kobayashi.onrender.com", { 
             method: "POST",
             headers:{
                 "Content-Type": "application/json"
